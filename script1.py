3daf49aa44cb9ba65858e2bdc5c1fb8af5888b47
uhirpara <122479361+uhirpara@users.noreply.github.com>
2023-01-23 13:05:23 -0500

    modified both file in the repo clone
    deleted a print statement from script.py
    added two print statement to script2.py

--- script1.py
+++ script1.py
@@ -1,3 +1,2 @@
 # print line from zen of python
 print("There should be one-- and preferably only one -- obevious way to do it.")
-print("Although that way may not be obevious at first unless you're Dutch")
--- script2.py
+++ script2.py
@@ -1,2 +1,4 @@
 # This is good programming advice
 print("If the implementation is hard to exolain, it's a bad idea")
+print("Readability counts.")
+print("Although that way may not be obevious at first unless you're Dutch")
